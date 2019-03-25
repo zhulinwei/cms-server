30d99b4590562dfb5213b7386940f895f69c2d34
Level.Z <1171567018@qq.com>
2019-03-25 10:31:36 +0800

    Update mongodb.js

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -2,37 +2,21 @@ const config = require('../configs').mongodb;
 const MongoClient = require('mongodb').MongoClient;
 
 class Mongodb {
-  constructor() {
+  constructor () {
     this.dbs = {};
   }
- 
-  // 允许连接多个Mongodb库 
-  async __connection(dbs) {
-    if (!dbs || dbs.length < 1) return [];
-    return await Promise.all(dbs.map(db=> {
-      const url = config[db].url || '';
-      const options = config[db].options || {};
-      return MongoClient.connect(url, options);
-    }));
-  }
-
-  async init() {
+  async init () {
     if (Object.keys(this.dbs).length > 0) return this.dbs;
-    const dbs = Object.keys(config);
-    const clients = await this.__connection(dbs).catch(err => {
-      // TODO 错误处理
-      console.log(err);
-      return [];
-    });
 
-    dbs.forEach((db, index) => {
-      if (clients[index] instanceof MongoClient) {
-        this.dbs[db] = clients[index].db(db);
-      }
-    });
+    const keys = Object.keys(config);
+    await Promise.all(keys.map(async key => {
+      let url = config[key].url || '';
+      let options = config[key].options || {};
+      let client = await MongoClient.connect(url, options);
+      this.dbs[key] = client.db(key);
+    }));
     return this.dbs;
   }
 }
 
 module.exports = new Mongodb();
-
