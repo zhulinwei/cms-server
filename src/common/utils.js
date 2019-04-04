24498a0e3bb8579e04b8862e28e6567d41f9bd09
Level.Z <1171567018@qq.com>
2019-04-04 11:03:53 +0800

    add utils

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const crypto = require('crypto');
+const archiver = require('archiver');
 
-class Helper {
+class Utils {
   md5 (content) {
     return crypto.createHash('md5').update(content).digest('hex');
   }
@@ -16,6 +18,15 @@ class Helper {
   strongPassword (key) {
     // return this.sha256(md5(password));
   }
+
+  zipFolder (dirpath, destpath) {
+    const archive = archiver('zip', {
+      zlib: { level: 9 }
+    });
+    archive.directory(dirpath, false);
+    archive.pipe(fs.createWriteStream(destpath));
+    archive.finalize();
+  }
 }
 
-module.exports = new Helper();
+module.exports = new Utils();
