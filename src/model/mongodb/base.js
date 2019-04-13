cacedcf2579fc1be126397337a11497ef2cbc7ac
Level.Z <1171567018@qq.com>
2019-04-13 20:23:10 +0800

    Update base.js

--- src/model/mongodb/base.js
+++ src/model/mongodb/base.js
@@ -1,82 +1,70 @@
 const mongodb = require('../../database').mongodb;
 
 class BaseModel {
-  constructor(database, collection) {
+  constructor (database, collection) {
     if (!database) throw new Error('database name is not defined');
-    if (!collection) throw new Error('collection name is not defined')
+    if (!collection) throw new Error('collection name is not defined');
     this.coll = mongodb[database].collection(collection);
   }
 
-  async insertOne(doc, options) {
-    doc = doc || {};
-    options = options || null;
-    return await this.coll.insertOne(doc, options);
+  async insertOne (doc = {}, options = {}) {
+    const result = await this.coll.insertOne(doc, options);
+    return result;
   }
 
-  async findOne(query, options) {
-    query = query || {};
-    options = options || null;
-    return await this.coll.findOne(query, options);
+  async findOne (query = {}, options = {}) {
+    const result = await this.coll.findOne(query, options);
+    return result;
   }
 
-  async find(query, options) {
-    query = query || {};
-    options = options || null;
-    return await this.coll.find(query, options).toArray();
+  async find (query = {}, options = {}) {
+    const result = await this.coll.find(query, options).toArray();
+    return result;
   }
 
-  async findOneAndUpdate(filter, update, options) {
-    filter = filter || {};
-    update = update || {};
-    options = options || null;
-    return await this.coll.findOneAndUpdate(filter, update, options);
+  async findOneAndUpdate (filter = {}, update = {}, options = {}) {
+    const result = await this.coll.findOneAndUpdate(filter, update, options);
+    return result;
   }
 
-  async count(query, options) {
-    query = query || {};
-    options = options || null;
-    return await this.coll.count(query, options);
+  async count (query = {}, options = {}) {
+    const result = await this.coll.count(query, options);
+    return result;
   }
 
-  async updateOne(filter, update, options) {
-    filter = filter || {};
-    update = update || {};
-    options = options || null;
-    return await this.coll.updateOne(filter, update, options);
-  }
-  
-  async updateMany(filter, update, options) {
-    filter = filter || {};
-    update = update || {};
-    options = options || null;
-    return await this.coll.updateMany(filter, update, options);
+  async updateOne (filter = {}, update = {}, options = {}) {
+    const result = await this.coll.updateOne(filter, update, options);
+    return result;
   }
 
-  async deleteOne(filter, options) {
-    return await this.coll.deleteOne(filter, options);
-  }
-  
-  async deleteMany(filter, options) {
-    return await this.coll.deleteMany(filter, options);
+  async updateMany (filter = {}, update = {}, options = {}) {
+    const result = await this.coll.updateMany(filter, update, options);
+    return result;
   }
 
-  async distinct(key, query, options) {
-    key = key || '';
-    query = query || {};
-    options = options || null;
-    return await this.coll.distinct(key, query, options);
+  async deleteOne (filter = {}, options = {}) {
+    const result = await this.coll.deleteOne(filter, options);
+    return result;
   }
 
-  async aggregate(pipeline, options) {
-    pipeline = pipeline || {};
-    options = options || null;
-    return await this.coll.aggregate(pipeline, options).toArray();
+  async deleteMany (filter = {}, options = {}) {
+    const result = await this.coll.deleteMany(filter, options);
+    return result;
   }
 
-  async bulkWrite(operations, options) {
-    operations = operations || [];
-    options = options || null;
-    return await this.coll.bulkWrite(operations, options);
+  async distinct (key = '', query = {}, options = {}) {
+    const result = await this.coll.distinct(key, query, options);
+    return result;
+  }
+
+  async aggregate (pipeline = {}, options = {}) {
+    const result = await this.coll.aggregate(pipeline, options).toArray();
+    return result;
+  }
+
+  async bulkWrite (operations = [], options = {}) {
+    const result = await this.coll.bulkWrite(operations, options);
+    return result;
   }
 }
 
