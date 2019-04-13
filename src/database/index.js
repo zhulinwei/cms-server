e6552a337d97af16ee8073928428460ae89c384f
Level.Z <1171567018@qq.com>
2019-04-13 20:09:55 +0800

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,22 +1,22 @@
 const redis = require('./redis');
 const mongodb = require('./mongodb');
 
-class Db {
-  constructor() {
+class Database {
+  constructor () {
     this.redis = {};
     this.mongodb = {};
   }
 
-  async init(){
+  async init () {
     try {
       this.redis = await redis.init();
       console.log('redis初始化成功');
       this.mongodb = await mongodb.init();
       console.log('mongodb初始化成功');
-    } catch (err) {
-      console.log(err);  
+    } catch (error) {
+      console.log(error);
     }
   }
 }
 
-module.exports = new Db();
+module.exports = new Database();
