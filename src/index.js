55f84684b9c27296992bbbe037424d8e676e32d4
Level.Z <1171567018@qq.com>
2019-04-28 14:49:01 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 const moment = require('moment');
-const config = require('./configs');
+const configs = require('./configs');
 const service = require('./server');
 const mainService = require('./service/mail');
 
 service.start()
   .then(async app => {
-    app.listen(config.server.port);
+    app.listen(configs.server.port);
     const now = moment().format('YYYY-MM-DD HH:mm:ss');
-    await mainService.send({ subject: 'cms系统推送通知', html: `服务启动成功，启动时间：${now}` }); 
+    // if (!configs.server.development) await mainService.send({ subject: 'cms系统推送通知', html: `服务启动成功，启动时间：${now}` });
     console.log(`服务启动成功，启动时间：${now}`);
-  }).catch(async err => {
-    console.log(err);
-    await mainService.send({ subject: 'cms系统错误警报', html: err }); 
-    console.log(`服务启动失败`);
+  }).catch(async error => {
+    // if (!configs.server.development) await mainService.send({ subject: 'cms系统错误警报', html: error });
+    console.log(`服务启动失败: `, error);
   });
-
