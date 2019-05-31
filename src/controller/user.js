0ae3cedd40154c79a6dfe74260152e0fd4d8b018
Level.Z <1171567018@qq.com>
2019-05-31 23:22:27 +0800

    Update user.js

--- src/controller/user.js
+++ src/controller/user.js
@@ -2,6 +2,8 @@ const utils = require('l-utility');
 const service = require('../service');
 const createError = require('http-errors');
 
+const debug = require('debug')('cms:contoller:user');
+
 class UserController {
   async setCookies (ctx, next) {
     const user = ctx.state.user;
@@ -10,27 +12,49 @@ class UserController {
     const expires = new Date(Date.now() + 2 * 3600 * 1000);
     ctx.cookies.set('uid', user.uid, { domain, expires, httpOnly: false, overwrite: true });
     ctx.cookies.set('role', user.role, { domain, expires, httpOnly: false, overwrite: true });
+
+    delete ctx.state.user;
     await next();
   }
 
+  /*
+   * 1.检查是否登录成功
+   * 2.检查是否为新用户
+   * 3.检查是否需重定向
+   */
   async login (ctx, next) {
-    const referer = ctx.query.referer;
+    const passportUser = ctx.state.passport;
+    if (!passportUser) throw new createError.BadRequest('无效的用户信息~');
+
+    const isNewUser = await service.user.isNewUser(passportUser);
+    if (isNewUser) {
+      ctx.state.user = await service.user.saveNewUser(passportUser);
+    } else {
+      ctx.state.user = await service.user.getUserByPassport(passportUser);
+    }
+
+    delete ctx.state.passport;
+    await next();
+
+    const referer = ctx.query.state;
     if (referer) return ctx.redirect(referer);
-    ctx.body = ctx.state.user;
+    else ctx.body = ctx.state.user;
   }
 
-  async getAuthorizeUrl (ctx, next) {
-    const type = ctx.params.type;
-    ctx.body = service.user.getAuthorizeUrl(type);
-  }
+  async getUserinfo (ctx, next) {
+    const uid = ctx.cookies.get('uid') || ctx.request.body.uid;
+    const role = ctx.cookies.get('role') || ctx.request.body.role;
+    debug(`uid: ${uid} - role: ${role}`);
 
-  async authorization (ctx, next) {
-    const user = ctx.state.passport;
-    console.log(user);
-    if (!user) throw new createError.BadRequest('无效的用户信息~');
-    const isNewUser = await service.user.isNewUser(user);
-    console.log(isNewUser);
-    if (isNewUser) await service.user.register(user);
+    let user = {};
+    if (uid && service.role.isUser(role)) user = await service.user.getUserById(uid);
+    if (uid && service.role.isTourist(role)) user = await service.user.getTouristById(uid);
+
+    if (!user.uid) {
+      user = await service.user.buildTourist();
+      ctx.state.user = user;
+      await next();
+    }
     ctx.body = user;
   }
 }
